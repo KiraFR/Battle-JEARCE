4a8600cb077af30db0ea5a9ca600017c96ebd8d3
AdamPoux <48692403+AdamPoux@users.noreply.github.com>
2019-03-21 15:23:10 +0100

    Obstacles can't block the game
    Les obstacles sont générés aléatoirement mais ne bloquent désormais plus les spawns des personnages + le jeu en lui-même (sur une 20aine de générations en tout cas)
    
    https://github.com/kira370/Battle-JEARCE/issues/7

--- Unity/ProjetUnity/Assets/Scripts/BoardManager.cs
+++ Unity/ProjetUnity/Assets/Scripts/BoardManager.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
+
     public int columns = 8;
     public int rows = 8;
     public int nbObstacles;
@@ -17,6 +18,9 @@ public class BoardManager : MonoBehaviour
 
     private List<Vector3> gridPositions = new List<Vector3>();
     private Transform boardHolder;
+    private List<GameObject> obstacles = new List<GameObject>();    //Global list so we can use it in the isBlocked() method
+
+
     void InitialiseList()
     {
         gridPositions.Clear();
@@ -28,6 +32,8 @@ public class BoardManager : MonoBehaviour
             }
         }
     }
+
+
     void BoardSetup()
     {
         boardHolder = new GameObject("Board").transform;
@@ -50,19 +56,71 @@ public class BoardManager : MonoBehaviour
         }
     }
 
-    void ObstaclesSetup()
+    //Method that will first add obstacles randomly before checking is the board is blocked
+    //If so then it'll call the method isBlocked() that will remove one obstacle to set the board playable
+    void ObstaclesSetup()   
     {
+
+        obstacles.Clear();
+
         for (int i = 0; i < nbObstacles; i++)
         {
-            int randomIndex = UnityEngine.Random.Range(0, gridPositions.Count);
-            Vector3 randomPosition = gridPositions[randomIndex];
-            GameObject floor = GetGameObject((int)randomPosition.x, (int)randomPosition.y);
-            if(floor != null)
+            int x = UnityEngine.Random.Range(0, columns);   //Choosing a random x
+            int y = UnityEngine.Random.Range(2, rows - 2);  //Choosing a random y letting some space for the spawn of characters
+
+            Vector3 randomPosition = new Vector3(x, y, 0f);
+
+            GameObject obstacle = GetGameObject((int)randomPosition.x, (int)randomPosition.y);
+            obstacles.Add(obstacle);    //Adding our obstacle in the global obstacles list
+
+            if(obstacle != null)
             {
-                floor.GetComponent<SpriteRenderer>().sprite = floor.GetComponent<Square>().inaccessibleSprite;               
+                obstacle.GetComponent<SpriteRenderer>().sprite = obstacle.GetComponent<Square>().inaccessibleSprite;               
+            }
+
+           
+            if((int)obstacle.transform.position.x == 1)  //Condition to verify to start the isBlocked() method
+            {
+                int increment = 1;  //increment that will count the number of obstacles that can block the board (blocked if increment = columns)
+                isBlocked(obstacle, increment);
+            }
+
+        }
+    }
+
+
+    //Recursive method that will check is there is a line of obstacle blocking the way
+    //If so the last blocking obstacle will be removed
+    void isBlocked(GameObject obstacle, int increment) 
+    {
+
+        int xObs = (int)obstacle.transform.position.x;
+        int yObs = (int)obstacle.transform.position.y;
+
+        if (increment == columns) //Is the game is bocked the last blocking obstacle will be removed and replaced by a normal floor
+        {
+            obstacle.GetComponent<SpriteRenderer>().sprite = obstacle.GetComponent<Square>().baseSprite; 
+        }
+
+        else 
+        {
+            for(int i = 0; i < obstacles.Count; i++)
+            {
+
+                int xObsi = (int)obstacles[i].transform.position.x;
+                int yObsi = (int)obstacles[i].transform.position.y;
+
+                //If there is another potential blocking obstacle then
+                if (xObsi == xObs + 1 && (yObsi == yObs -1 || yObsi == yObs || yObsi == yObs + 1))
+                
+                {
+                    increment++;
+                    isBlocked(obstacles[i], increment);
+                }
             }
         }
     }
+
 
 
     public void SetupScene()
