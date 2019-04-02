5bfddf6231ec56107b6e1db479fc189b1a708f2d
ErwaNegaret <48562989+ErwaNegaret@users.noreply.github.com>
2019-04-02 15:42:32 +0200

    Comfirmation for end turn
    - Ajout d'un canvas pour le texte et les deux boutons
    - Ajout d'une fonction sur le bouton end turn permettant l'affichage du canvas
    - Ajout d'une fonction sur le bouton "Oui" appellant EndTurn et cache le canvas
    - Ajout d'une fonction sur le bouton "Non" cachant le canvas

--- Unity/ProjetUnity/Assets/Scripts/ButtonManager.cs
+++ Unity/ProjetUnity/Assets/Scripts/ButtonManager.cs
@@ -1,11 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
+    public GameObject confirmationCanvas;
+    public Button yesButton;
+    public Button noButton;
+
+    public void Start()
+    {
+        yesButton.onClick.AddListener(EndTurn);
+        noButton.onClick.AddListener(StayTurn);
+    }
+
     public void EndTurn()
     {
         GameManager.instance.EndTurn();
+        confirmationCanvas.SetActive(false);
+    }
+
+    public void StayTurn()
+    {
+        confirmationCanvas.SetActive(false);
+    }
+
+    public void Confirmation()
+    {
+        confirmationCanvas.SetActive(true);
     }
 }
