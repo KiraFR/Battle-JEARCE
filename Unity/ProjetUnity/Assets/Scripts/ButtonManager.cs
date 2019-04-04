403224ee8e1d37dea7e4c7383153ebfedfe0ce88
ErwaNegaret <48562989+ErwaNegaret@users.noreply.github.com>
2019-04-04 12:58:56 +0200

    Add a canvas for the start of the turn
    - Ajout d'un canvas signalant le début du tour d'un joueur
    - Affichage du canvas pendant 2 secondes puis sa disparition

--- Unity/ProjetUnity/Assets/Scripts/ButtonManager.cs
+++ Unity/ProjetUnity/Assets/Scripts/ButtonManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
+    public GameObject turnCanvas;
     public GameObject confirmationCanvas;
     public Button yesButton;
     public Button noButton;
@@ -13,12 +14,21 @@ public class ButtonManager : MonoBehaviour
     {
         yesButton.onClick.AddListener(EndTurn);
         noButton.onClick.AddListener(StayTurn);
+        StartCoroutine("TurnStart");
+    }
+
+    IEnumerator TurnStart()
+    {
+        turnCanvas.SetActive(true);
+        yield return new WaitForSeconds(2);
+        turnCanvas.SetActive(false);
     }
 
     public void EndTurn()
     {
         GameManager.instance.EndTurn();
         confirmationCanvas.SetActive(false);
+        StartCoroutine("TurnStart");
     }
 
     public void StayTurn()
