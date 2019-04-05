ebdfe6b9e877c62dd38deac1243c8e5c720ab50f
kira370 <thekirahd@gmail.com>
2019-04-05 13:40:38 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/kira370/Projet-Unity-Web""
    This reverts commit f0048ca5090571b11de8edfd7dca5194128213d4.

--- Unity/ProjetUnity/Assets/Scripts/ButtonManager.cs
+++ Unity/ProjetUnity/Assets/Scripts/ButtonManager.cs
@@ -5,20 +5,40 @@ using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
-    public GameObject confirmationCanvas;
-    public Button yesButton;
-    public Button noButton;
-
+    private GameObject turnCanvas;
+    private GameObject confirmationCanvas;
+    private Button yesButton;
+    private Button noButton;
+    private Button EndTurnBtn;
     public void Start()
     {
+
+        turnCanvas = GameObject.Find("CanvasTurn");
+        confirmationCanvas = GameObject.Find("CanvasConfirm");
+        EndTurnBtn = GameObject.Find("CanvasBottom").transform.Find("EndTurn").GetComponent<Button>();
+        yesButton = confirmationCanvas.transform.Find("YesButton").GetComponent<Button>();
+        noButton = confirmationCanvas.transform.Find("NoButton").GetComponent<Button>();
+
         yesButton.onClick.AddListener(EndTurn);
         noButton.onClick.AddListener(StayTurn);
+        EndTurnBtn.onClick.AddListener(Ready);
+
+        confirmationCanvas.SetActive(false);
+        turnCanvas.SetActive(false);
+    }
+
+    IEnumerator TurnStart()
+    {
+        turnCanvas.SetActive(true);
+        yield return new WaitForSeconds(2);
+        turnCanvas.SetActive(false);
     }
 
     public void EndTurn()
     {
         GameManager.instance.EndTurn();
         confirmationCanvas.SetActive(false);
+        StartCoroutine("TurnStart");
     }
 
     public void StayTurn()
@@ -29,5 +49,19 @@ public class ButtonManager : MonoBehaviour
     public void Confirmation()
     {
         confirmationCanvas.SetActive(true);
+    }
+
+    public void Ready()
+    {
+        GameManager.instance.StartGame();
+        /*
+         * 
+         * Choses à faire entre les deux => Multijoueur
+         * 
+         * */
+
+        StartCoroutine("TurnStart");
+
+        GameObject.Find("EndTurn").GetComponent<Button>().onClick.AddListener(EndTurn);
     }
 }
