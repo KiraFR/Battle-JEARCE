09f066f00d702dd2b5acefc7c0b34260790bab63
kira370 <thekirahd@gmail.com>
2019-04-08 18:30:55 +0200

    server init

---
+++ Unity/ServerBattleJEARCE/ClientTest/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace MultiClient
+{
+    class Program
+    {
+        private static readonly Socket ClientSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private const int PORT = 8181;
+        private static Thread receiveRespThread;
+        static void Main()
+        {
+            Console.Title = "Client";
+            ConnectToServer();
+            RequestLoop();
+            Exit();
+        }
+
+        private static void ConnectToServer()
+        {
+            int attempts = 0;
+
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    Console.WriteLine("Connection attempt " + attempts);
+                    ClientSocket.Connect(IPAddress.Loopback, PORT);
+                }
+                catch (SocketException) 
+                {
+                    Console.Clear();
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Connected");
+        }
+
+        private static void RequestLoop()
+        {
+            Console.WriteLine(@"<Type ""exit"" to properly disconnect client>");
+
+            receiveRespThread = new Thread(new ThreadStart(ReceiveResponse));
+            receiveRespThread.Start();
+            while (true)
+            {
+                SendRequest();
+            }
+        }
+
+        private static void Exit()
+        {
+            SendString("exit");
+            ClientSocket.Shutdown(SocketShutdown.Both);
+            ClientSocket.Close();
+            Environment.Exit(0);
+        }
+
+        private static void SendRequest()
+        {
+            string request = Console.ReadLine();
+            SendString(request);
+
+            if (request.ToLower() == "exit")
+            {
+                Exit();
+                receiveRespThread.Abort();
+            }
+        }
+
+        private static void SendString(string text)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private static void ReceiveResponse()
+        {
+            while (true)
+            {
+                var buffer = new byte[2048];
+                int received = ClientSocket.Receive(buffer, SocketFlags.None);
+                if (received > 0)
+                {
+                    var data = new byte[received];
+                    Array.Copy(buffer, data, received);
+                    string text = Encoding.ASCII.GetString(data);
+                    Console.WriteLine(text);
+                }
+            }
+        }
+    }
+}
---
+++ Unity/ServerBattleJEARCE/ServerBattleJEARCE/Game.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServeurJEARCE
+{
+    class Game
+    {
+        private Socket player_one;
+        private Socket player_two;
+        private const int BUFFER_SIZE = 2048;
+        private static byte[] buffer = new byte[BUFFER_SIZE];
+        private static bool playing;
+
+        public Game(Socket p1, Socket p2)
+        {
+            player_one = p1;
+            player_two = p2;
+            playing = true;
+            Console.WriteLine("Two clients Connected");
+            player_one.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceiveCallBackPlayerOne, player_one);
+            player_two.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallBackPlayerTwo, player_two);
+        }
+
+        public void ReceiveCallBackPlayerTwo(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = socket.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Client forcefully disconnected");
+                socket.Close();
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+            Console.WriteLine("Player two send text: " + text);
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            player_one.Send(data);
+
+            socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallBackPlayerTwo, socket);
+        }
+
+        public void ReceiveCallBackPlayerOne(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = socket.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Client forcefully disconnected");
+                socket.Close();
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+            Console.WriteLine("Player one send text: " + text);
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            player_two.Send(data);
+
+            socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallBackPlayerOne, socket);
+        }
+    }
+}
