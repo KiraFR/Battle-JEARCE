8e05fe97ee5de82cff398d3be63b86d5ba3d1541
Elie Karaboghossian <Elie@MacBook-Air-de-Elie.local>
2019-04-17 17:59:20 +0200

    Requette Http

--- Unity/ProjetUnity/Assets/Scripts/ConnexionAplli.cs
+++ Unity/ProjetUnity/Assets/Scripts/ConnexionAplli.cs
@@ -2,24 +2,35 @@
 using UnityEngine.UI;
 using System.Security.Cryptography;
 using System.Text;
+using System.Net;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Collections.Generic;
+
 public class ConnexionAplli : MonoBehaviour
 {
     public GameObject gomail;
     public GameObject gopwd;
 
+    private static readonly HttpClient client = new HttpClient();
+    private string mail;
+    private string mdp;
+
     public void ButtonClick()
     {
-        string mail = gomail.GetComponent<InputField>().text;
-        string mdp = gopwd.GetComponent<InputField>().text;
+        mail = gomail.GetComponent<InputField>().text;
+        mdp = gopwd.GetComponent<InputField>().text;
 
-        string key = "fUjXn2r5u7x!A%D*";
+        /*string key = "fUjXn2r5u7x!A%D*";
         byte[] tabKey = System.Text.Encoding.UTF8.GetBytes(key);
         byte[] data = Encoding.UTF8.GetBytes(mdp);
 
         byte[] enc = Encrypt(data, tabKey);
+        string result = Encoding.UTF8.GetString(enc);*/
 
-        string result = System.Text.Encoding.UTF8.GetString(enc);
-        Debug.Log(result);
+        RequetteHttpAsync();
+
     }
 
     public static byte[] Encrypt(byte[] data, byte[] key)
@@ -32,5 +43,13 @@ public class ConnexionAplli : MonoBehaviour
             ICryptoTransform encrypter = csp.CreateEncryptor();
             return encrypter.TransformFinalBlock(data, 0, data.Length);
         }
+    }
+
+    public async System.Threading.Tasks.Task RequetteHttpAsync()
+    {
+        var values = new Dictionary<string, string> { { "mail", mail }, { "mdp", mdp } };
+        var content = new FormUrlEncodedContent(values);
+        var response = await client.PostAsync("http://localhost:8080", content);
+        var responseString = await response.Content.ReadAsStringAsync();
     }
 }
