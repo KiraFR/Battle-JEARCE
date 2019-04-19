6b2330e4c5a585f20b06fea5614dcc3b56ad6d00
Kira <jimmydelannoy@gmail.com>
2019-04-19 11:55:47 +0200

    Menu Scene

---
+++ Unity/ProjetUnity/Assets/Scripts/ClickButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickButton : MonoBehaviour
+{
+
+    public GameObject canvasForOptionCanvas;
+    public GameObject buttonForOptionCanvas;
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void ShowCanvas(Canvas canvas)
+    {
+        canvas.gameObject.SetActive(true);
+    }
+
+    public void clickUrl(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+
+    public void BackButton()
+    {
+        buttonForOptionCanvas.GetComponent<Button>().onClick.AddListener(() => canvasForOptionCanvas.SetActive(true));
+    }
+}
--- Unity/ProjetUnity/Assets/Scripts/MenuLoader.cs
+++ Unity/ProjetUnity/Assets/Scripts/MenuLoader.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class MenuLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject FormationPanel;
+    public GameObject OptionsPanel;
+    public GameObject PlayPanel;
+    public GameObject MainMenuPanel;
+    public GameObject ConnexionPanel;
+
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        FormationPanel.SetActive(false);
+        OptionsPanel.SetActive(false);
+        PlayPanel.SetActive(false);
+        MainMenuPanel.SetActive(true);
+        ConnexionPanel.SetActive(false);
     }
 }
